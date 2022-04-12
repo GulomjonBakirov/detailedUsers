5cc3cb4761011787d335b87acb28e11f601b7d67
GulomjonBakirov <bakirovgulomjon@gmail.com>
2022-04-12 20:14:41 +0500

    refactor: Home component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect } from "react";
-import { Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Layout, Card, Avatar, Alert, Pagination } from "antd";
+
 import { useDispatch, useSelector } from "react-redux";
 
 import { getAllUser } from "../store/actions/userActions";
+import Loader from "./Layout/Loader";
+import { Link } from "react-router-dom";
 
 const { Content } = Layout;
+const { Meta } = Card;
 
 export default function Main() {
-  // const { loading, error, users } = useSelector((state) => state.allUser);
+  const { loading, error, data, users } = useSelector((state) => state.users);
+  const [current, setCurrent] = useState(1);
+
   const dispatch = useDispatch();
 
+  const onClose = (e) => {
+    console.log(e, "I was closed.");
+  };
+
   useEffect(() => {
-    dispatch(getAllUser());
-  }, [dispatch]);
+    dispatch(getAllUser(current));
+
+    if (error) {
+      return (
+        <Alert
+          message="Error Text"
+          description={`${error.message}`}
+          closable
+          onClose={onClose}
+        />
+      );
+    }
+  }, [dispatch, current, error]);
+
+  const onChange = (page) => {
+    console.log(page);
+    setCurrent(page);
+  };
 
   return (
     <Content
@@ -23,7 +49,47 @@ export default function Main() {
         minHeight: 280,
       }}
     >
-      Content
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="users">
+          {users?.map((user) => (
+            <Card
+              key={user.last_name}
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="userImage"
+                  width="300"
+                  height="300"
+                  src={`${user.avatar}`}
+                />
+              }
+              actions={[
+                <Button type="primary" key="moreInfo">
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={`user/${user.id}`}
+                  >
+                    More Information
+                  </Link>
+                </Button>,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src={`${user.avatar}`} />}
+                title={`${user.first_name} ${user.last_name}`}
+              />
+            </Card>
+          ))}
+          <Pagination
+            pageSize={6}
+            current={data.page}
+            onChange={onChange}
+            total={data.total}
+          />
+        </div>
+      )}
     </Content>
   );
 }
